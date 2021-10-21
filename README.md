7e4b6559a84c019013ed46f1c1946a7f28809322
VofSwords <47701851+VofSwords@users.noreply.github.com>
2021-10-22 00:52:53 +0300

    main page created

---
+++ README.md
@@ -0,0 +1,2 @@
+## LICENSE
+[AGPL-3.0-or-later](https://github.com/VofSwords/vofswords.github.io/blob/master/LICENSE)
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+//  *******************************************
+//  * VofSwords' personal portfolio website   *
+//  * Copyright (C) 2021-present VofSwords    *
+//  *******************************************
+
+import logo from './brending/blue-smooth-logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h1>VofSwords</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
