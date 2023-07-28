3993aa3e5463cae074fd7c3fc752ad00543686cc
VofSwords <47701851+VofSwords@users.noreply.github.com>
2023-07-28 19:11:36 +0500

    feat: expirience display

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,12 @@
 import { Head } from "$fresh/runtime.ts";
+import { difference } from "$std/datetime/mod.ts";
 
 const Main = () => {
+  const expirienceStart = new Date("2018-07-01T00:00:00");
+  const { years: yearsOfExpirience } = difference(expirienceStart, new Date(), {
+    units: ["years"],
+  });
+
   return (
     <>
       <Head>
@@ -8,8 +14,14 @@ const Main = () => {
       </Head>
       <main>
         <h2 class="text-xl font-bold">
-          Creating exceptional digital experiences
+          Building your success
         </h2>
+        <section>
+          <p>
+            <b class="font-bold">{yearsOfExpirience} years</b> of{" "}
+            <b class="font-bold">experience</b> just makes it
+          </p>
+        </section>
       </main>
     </>
   );
